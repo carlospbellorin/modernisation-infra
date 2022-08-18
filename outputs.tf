8008fd33cf3889739608e75da360f5fbefafb11f
carlospbellorin <carlos.pinto@hainesattract.co.nz>
2022-08-18 13:51:54 -0400

    Provisioned User Assigned Identity for pulling images.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-# Resource Group Outputs:
+# [START] Resource Group Outputs:
 output "resource_group_name" {
   description = "Name of the resource group"
   value       = var.resource_group_name
@@ -16,8 +16,9 @@ output "resource_group_tags" {
   description = "Resource Group Tags"
   value       = azurerm_resource_group.rg.tags
 }
+# [END] Resource Group Outputs
 
-# Azure Container Registry Outputs:
+# [START] Azure Container Registry Outputs:
 output "azure_container_registry_name" {
   description = "Name of the Azure Container Registry"
   value       = azurerm_container_registry.acr.name
@@ -35,3 +36,25 @@ output "azure_container_registry_tags" {
   description = "Tags of the Azure Container Registry"
   value       = azurerm_container_registry.acr.tags
 }
+# [END] Azure Container Registry Outputs
+
+# [START] Assigned Identity Outputs:
+output "user_assigned_identity_name" {
+  description = "Name of the Assigned Identity"
+  value       = azurerm_user_assigned_identity.uai-pull.name
+  depends_on  = [azurerm_user_assigned_identity.uai-pull]
+}
+output "user_assigned_identity_client_id" {
+  description = "ID of the Assigned Identity"
+  value       = azurerm_user_assigned_identity.uai-pull.client_id
+}
+output "user_assigned_identity_principal_id" {
+  description = "Principal ID of the Assigned Identity"
+  value       = azurerm_user_assigned_identity.uai-pull.principal_id
+}
+output "user_assigned_tenant_id" {
+  description = "Tenant ID of the Assigned Identity"
+  value       = azurerm_user_assigned_identity.uai-pull.tenant_id
+}
+# [END] Assigned Identity Outputs
+
