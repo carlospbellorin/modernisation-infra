f6f724a9a25a37905768e86151f382eee68b9f19
carlospbellorin <carlos.pinto@hainesattract.co.nz>
2022-08-18 11:38:10 -0400

    Fixed syntax warning on variables

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "resource_group_name" {
-  type          = "string"
+  type          = string
   description   = "Name of resource group"
   default       = "rg-paysystems-dev-001"
 }
 
 variable "location" {
-    type        = "string"
+    type        = string
     description = "Name of region"
     default     = "Australia East"
 }
 
 variable "environment" {
-  type          = "string"
+  type          = string
   description   = "Name of environment"
   default       = "dev"
 }
