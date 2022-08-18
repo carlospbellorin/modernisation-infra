b83b5ea5e626d81d3074bbd6a76fda9f90b93ec0
carlospbellorin <carlos.pinto@hainesattract.co.nz>
2022-08-18 11:23:38 -0400

    Resource group variables

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  type        = "string"
+  description = "Name of resource group"
+  default     = "rg-paysystems-dev-001"
+}
+
+variable "region" {
+    type        = "string"
+    description = "Name of region"
+    default     = "Australia East"
+}
